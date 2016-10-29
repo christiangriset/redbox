bfe5608a494f6e783f24d5bfa33cc3179bacde02
Christian Griset <christian.griset@clever.com>
2016-10-29 05:06:49 +0000

    Remove s3 logic from Redbox

--- redbox.go
+++ redbox.go
@@ -1,43 +1,21 @@
 package redbox
 
 import (
-	"encoding/json"
 	"fmt"
 	"sync"
-	"time"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/credentials"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/s3"
-)
-
-const (
-	// DefaultBufferSize is set to 10MB
-	DefaultBufferSize = 10000000
+	"github.com/cgclever/redbox/s3box"
 )
 
 var (
-	// ErrIncompleteDestinationConfig indicates that either a schema or table name are missing
-	ErrIncompleteDestinationConfig = fmt.Errorf("Destination config input must include a schema and table name.")
-
 	// ErrSendingInProgress captures operations when a send is in progress.
 	ErrSendingInProgress = fmt.Errorf("Cannot perform any action when sending is in progress.")
 
 	// ErrIncompleteArgs captures when not enough arguments are given for generating a new Redbox
 	ErrIncompleteArgs = fmt.Errorf("Creating a redshift box requires a schema, table and an s3 bucket.")
 
-	// ErrBoxIsSealed signals an operation which can't occur when a box is sealed.
-	ErrBoxIsSealed = fmt.Errorf("Cannot perform action when box is sealed.")
-
-	// ErrInvalidConfig indicates the config is invalid for sending
-	ErrInvalidConfig = fmt.Errorf("Cannot perform send with an invalid configuration. It's recommended you create a test to ensure valid configuration inputs.")
-
 	// ErrInvalidJSONInput captures when the input data can't be marshalled into JSON.
 	ErrInvalidJSONInput = fmt.Errorf("Only JSON-able inputs are supported for syncing to Redshift.")
-
-	// ErrBoxNotSealed captures trying to create a custom manifest on an unsealed stream
-	ErrBoxNotSealed = fmt.Errorf("Can only create a custom manifest on a sealed stream.")
 )
 
 // Redbox manages piping data into Redshift. The core idea is to buffer data locally, ship to s3 when too much is buffered, and finally box to Redshift.
@@ -51,29 +29,8 @@ type Redbox struct {
 	// table is the table name of the destination
 	table string
 
-	// s3Bucket specifies the intermediary bucket before ultimately piping to Redshift. The user should have access to this bucket.
-	s3Bucket string
-
-	// s3Handler manages the piping of data to s3
-	s3Handler *s3.S3
-
-	// bufferSize is the maximum size of data we're willing to buffer before creating an s3 file
-	bufferSize int
-
-	// bufferedData is the data currently buffered in the box. Calling Dump ships this data into s3
-	bufferedData []byte
-
-	// timestamp tracks the time a box was created or reset
-	timestamp time.Time
-
-	// fileNumber indicates the number of s3 files which have currently been created
-	fileNumber int
-
-	// fileLocations stores the s3 files already created
-	fileLocations []string
-
-	// isSealed indicates whether writes are currently allows to the buffer
-	isSealed bool
+	// s3Box manages the transport of data to Redshift.
+	s3Box s3box.S3Box
 
 	// SendingInProgress indicates if a send is in progress
 	SendingInProgress bool
@@ -81,8 +38,8 @@ type Redbox struct {
 	// truncate indicates if we should truncate the destination table
 	truncate bool
 
-	// force indicates whether we should force the data into redshift without the protection of checking for data duplication
-	force bool
+	// options remembers the options used to configure the instance
+	options NewReboxOptions
 }
 
 // NewRedboxOptions is the expected input for creating a new Redbox
@@ -93,7 +50,7 @@ type NewRedboxOptions struct {
 	// Table is the destination Redshift table name
 	Table string
 
-	// S3Bucket specifies the intermediary bucket before ultimately piping to Redshift. The user should have access to this bucket
+	// S3Bucket specifies the intermediary bucket before ultimately piping to Redshift. The user should have access to this bucket.
 	S3Bucket string
 
 	// AWSKey is the AWS ACCESS KEY ID
@@ -119,127 +76,31 @@ func NewRedbox(options NewRedboxOptions) (*Redbox, error) {
 		return nil, ErrIncompleteArgs
 	}
 
-	bufferSize := DefaultBufferSize
-	if options.BufferSize > 0 {
-		bufferSize = options.BufferSize
-	}
+	s3Box, err := s3box.NewS3Box(s3box.NewS3BoxOptions{
+		S3Bucket:    options.S3Bucket,
+		AWSKey:      options.AWSKey,
+		AWSPassword: options.AWSPassword,
+		AWSToken:    options.AWSToken,
+		BufferSize:  options.BufferSize,
+	})
 
-	// Setup s3 handler and aws configuration. If no creds are explicitly provided, they'll be grabbed from the environment.
-	region, err := getRegionForBucket(options.S3Bucket)
 	if err != nil {
-		return nil, fmt.Errorf("Failed to get AWS region for bucket %s: (%s)", options.S3Bucket, err)
+		return nil, err
 	}
-
-	// If AWS creds were provided use those, otherwise grab them from your environment
-	var awsCreds *credentials.Credentials
-	if options.AWSKey == "" && options.AWSPassword == "" && options.AWSToken == "" {
-		awsCreds = credentials.NewEnvCredentials()
-	} else {
-		awsCreds = credentials.NewStaticCredentials(options.AWSKey, options.AWSPassword, options.AWSToken)
-	}
-	awsConfig := aws.NewConfig().WithRegion(region).WithS3ForcePathStyle(true).WithCredentials(awsCreds)
-	awsSession := session.New()
 
 	return &Redbox{
-		schema:     options.Schema,
-		table:      options.Table,
-		s3Bucket:   options.S3Bucket,
-		timestamp:  time.Now(),
-		s3Handler:  s3.New(awsSession, awsConfig),
-		bufferSize: bufferSize,
-		truncate:   options.Truncate,
-		force:      options.Force,
+		schema:   options.Schema,
+		table:    options.Table,
+		s3Box:    s3Box,
+		truncate: options.Truncate,
+		options:  options,
 	}, nil
-}
-
-// NextBox gives you a new box, forgetting everything about previously packaged data
-func (rp *Redbox) NextBox() error {
-	if rp.SendingInProgress {
-		return ErrSendingInProgress
-	}
-	rp.Lock()
-	rp.timestamp = time.Now()
-	rp.fileNumber = 0
-	rp.fileLocations = []string{}
-	rp.bufferedData = []byte{}
-	rp.isSealed = false
-	rp.Unlock()
-	return nil
 }
 
 // Pack writes bytes into a buffer. Once that buffer hits capacity, the data is output to s3.
 // Any error will leave the buffer unmodified.
 func (rp *Redbox) Pack(data []byte) error {
-	if rp.SendingInProgress {
-		return ErrSendingInProgress
-	}
-	if rp.isSealed {
-		return ErrBoxIsSealed
-	}
-
-	// If the bytes aren't in JSON format, return an error
-	var tempMap map[string]interface{}
-	if err := json.Unmarshal(data, &tempMap); err != nil {
-		return ErrInvalidJSONInput
-	}
-
-	rp.Lock()
-	oldBuffer := rp.bufferedData // If write
-	data = append(data, '\n')    // Append a new line for text-editor readability
-	rp.bufferedData = append(rp.bufferedData, data...)
-	rp.Unlock()
-
-	// If we're hitting capacity, dump the results to s3.
-	// If shipping to s3 errors, don't modify the buffer.
-	if len(rp.bufferedData) > rp.bufferSize {
-		if err := rp.dumpToS3(); err != nil {
-			rp.Lock()
-			rp.bufferedData = oldBuffer
-			rp.Unlock()
-			return err
-		}
-	}
-
 	return nil
-}
-
-// Seal closes writes and flushes any buffered data to s3. Call Unseal to enable writing again.
-func (rp *Redbox) Seal() error {
-	if rp.SendingInProgress {
-		return ErrSendingInProgress
-	}
-
-	if err := rp.dumpToS3(); err != nil {
-		return err
-	}
-
-	rp.isSealed = true
-	return nil
-}
-
-// dumpToS3 ships buffered  data to s3 and increments the index with a clean slate of running data
-func (rp *Redbox) dumpToS3() error {
-	if len(rp.bufferedData) == 0 {
-		return nil
-	}
-	fileKey := fmt.Sprintf("%s_%s_%d_%d.json.gz", rp.schema, rp.table, rp.timestamp.Unix(), rp.fileNumber)
-	rp.Lock()
-	defer rp.Unlock()
-	if err := writeToS3(rp.s3Handler, rp.s3Bucket, fileKey, rp.bufferedData); err != nil {
-		return err
-	}
-	rp.fileNumber++
-	rp.bufferedData = []byte{}
-	fileName := fmt.Sprintf("s3://%s/%s", rp.s3Bucket, fileKey)
-	rp.fileLocations = append(rp.fileLocations, fileName)
-	return nil
-}
-
-// createAndUploadManifest creates a manifest with a default convention and uploads it to s3.
-func (rp *Redbox) createAndUploadManifest() error {
-	timestamp := rp.timestamp.Round(time.Hour).UTC().Format(time.RFC3339) //
-	defaultManifestName := fmt.Sprintf("%s_%s_%s.manifest", rp.schema, rp.table, timestamp)
-	return rp.CreateAndUploadCustomManifest(defaultManifestName)
 }
 
 // Send ships written data to the destination Redshift table.
@@ -248,16 +109,10 @@ func (rp *Redbox) Send() error {
 		return ErrSendingInProgress
 	}
 
-	// If no data was ever writen, then simply return
-	if rp.fileNumber == 0 && len(rp.bufferedData) == 0 {
-		return nil
-	}
-
 	// Kick off the s3-to-Redshift job
 	rp.SendingInProgress = true
 	// To be filled in
 	rp.SendingInProgress = false
 
-	rp.NextBox()
 	return nil
 }
